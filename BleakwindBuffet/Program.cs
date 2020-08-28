ca04692ed3d26e945f97b65edf5cd2f9fc246be3
John Solomon <jackfrost0@users.campus>
2020-08-28 11:42:54 -0500

    Add project files.

---
+++ BleakwindBuffet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BleakwindBuffet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
