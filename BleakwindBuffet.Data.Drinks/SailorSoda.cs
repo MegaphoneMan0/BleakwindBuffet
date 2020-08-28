aab0116bc60191ca3dff1cd531e76fbc9f0de69a
John Solomon <jackfrost0@ksu.edu>
2020-08-28 13:01:40 -0500

    Not sure how to do enums cuz i'm dumb

---
+++ BleakwindBuffet.Data.Drinks/SailorSoda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BleakwindBuffet.Data.Drinks
+{
+    /*
+     * Author: John Solomon
+     * Class name: SailorSoda
+     * Purpose: To track sailor soda
+     */
+    class SailorSoda
+    {
+
+        //standard
+        private double smallPrice = 1.42;
+        private double mediumPrice = 1.74;
+        private double largePrice = 2.07;
+
+        private uint smallCalories = 117;
+        private uint mediumCalories = 153;
+        private uint largeCalories = 205;
+
+        private List<string> specialInstructions;
+
+        private Enums.Size Size = Small;
+
+
+
+
+
+
+
+    }
+}
