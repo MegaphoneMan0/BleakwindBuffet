4607e86d0268d5d9f86c258070fa2e3c0144279b
John Solomon <jackfrost0@ksu.edu>
2020-10-15 20:05:06 -0500

    Started working on order

--- Data/Order.cs
+++ Data/Order.cs
@@ -5,14 +5,118 @@ using BleakwindBuffet.Data.Enums;
 using BleakwindBuffet.Data.Abstract;
 using System.ComponentModel;
 using BleakwindBuffet.Data.Interfaces;
-
+using System.Collections.Specialized;
+using System.Collections;
 
 namespace BleakwindBuffet.Data
 {
-    class Order
+    class Order : ICollection<IOrderItem>,INotifyCollectionChanged,INotifyPropertyChanged
     {
 
         List<IOrderItem> itemsInTheOrder;
+
+
+
+
+
+        public event CollectionChangeEventHandler CollectionChanged;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+
+
+
+        /// <summary>
+        /// Unsure what this does
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            //IEnumerator enumerator = itemsInTheOrder;
+
+            //I have no idea how this is supposed to work, so I'm just going to comment it out
+            /*
+            foreach (IOrderItem orderItem in itemsInTheOrder)
+            {
+                enumerator. = orderItem;
+            }
+            */
+
+            //return enumerator;
+
+
+            return (IEnumerator)GetEnumerator();
+
+
+        }
+
+         
+
+        /// <summary>
+        /// Unsure what this does
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<IOrderItem> GetEnumerator()
+        {
+            //IEnumerator<IOrderItem> enumerator;
+
+           
+
+
+        }
+
+
+        /// <summary>
+        /// if the order is or is not read only
+        /// </summary>
+        public bool IsReadOnly { get; set; }
+
+
+        /// <summary>
+        /// the amount of IOrderItems in itemsInTheOrder
+        /// </summary>
+        /// <returns></returns>
+        public int Count { get; set; }
+
+
+        /// <summary>
+        /// ICollection IOrderItem
+        /// </summary>
+        /// <param name="orderItems"></param>
+        /// <param name="i"></param>
+        public void CopyTo(IOrderItem[] orderItems, int i)
+        {
+
+        }
+
+        /// <summary>
+        /// checks the list to see if it contains the given IOrderItem
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(IOrderItem item)
+        {
+            if (itemsInTheOrder.Contains(item))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// ICollection's Clear()
+        /// </summary>
+        public void Clear()
+        {
+
+        }
 
         /// <summary>
         /// default constructor, sets the list to a new list
@@ -38,10 +142,27 @@ namespace BleakwindBuffet.Data
         /// removes an item from the list
         /// </summary>
         /// <param name="item"></param>
-        public void Remove (IOrderItem item)
+        public bool Remove(IOrderItem item)
         {
-            itemsInTheOrder.Remove(item);
+            if (Contains(item))
+            {
+                itemsInTheOrder.Remove(item);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+
+
+
+        
+
+
+
+
+
 
 
     }
